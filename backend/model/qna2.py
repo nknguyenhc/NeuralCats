4446388d5fcc436af9d939e6a60ecfb70a995a2d
kaitinghh <114789615+kaitinghh@users.noreply.github.com>
2023-12-03 23:49:45 +0800

    style fixes

--- backend/model/qna2.py
+++ backend/model/qna2.py
@@ -7,34 +7,34 @@ def prompt(content, numQns, difficulty):
     client = OpenAI()
 
     completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": content},
-        {"role": "user", "content": f"""Generate {numQns} multiple-choice questions with 4 options each based on the following article: \
-        Format the questions in this way: \
-        Questions:
-        1. [Question 1]
-            a. [Option 1]
-            b. [Option 2]
-            c. [Option 3]
-            d. [Option 4]
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": content},
+            {"role": "user", "content": f"""Generate {numQns} multiple-choice questions with 4 options each based on the following article: \
+            Format the questions in this way: \
+            Questions:
+            1. [Question 1]
+                a. [Option 1]
+                b. [Option 2]
+                c. [Option 3]
+                d. [Option 4]
 
-        2. [Question 2]
-            a. [Option 1]
-            b. [Option 2]
-            c. [Option 3]
-            d. [Option 4]
+            2. [Question 2]
+                a. [Option 1]
+                b. [Option 2]
+                c. [Option 3]
+                d. [Option 4]
 
-        ...
+            ...
 
-        Answer Key:
-        1. Correct Answer for Question 1
-        2. Correct Answer for Question 2
-        ...
+            Answer Key:
+            1. Correct Answer for Question 1
+            2. Correct Answer for Question 2
+            ...
 
-        Ensure that the questions cover a range of topics and are of {difficulty} difficulty level.
-        """}
-    ]
+            Ensure that the questions cover a range of topics and are of {difficulty} difficulty level.
+            """}
+        ]
     )
     return completion.choices[0].message.content
 
