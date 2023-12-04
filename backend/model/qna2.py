954d22e75116940c21e79d637a046f9bc4f73798
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2023-12-04 18:17:38 +0700

    Backend for user dashboard
    Added indentation to the prompt to ensure
    that OpenAI respond with the correct format.

--- backend/model/qna2.py
+++ backend/model/qna2.py
@@ -12,27 +12,27 @@ def prompt(content, numQns, difficulty):
             {"role": "system", "content": content},
             {"role": "user", "content": f"""Generate {numQns} multiple-choice questions with 4 options each based on the following article: \
 Format the questions in this way: \
-Questions:
-~question~ 1. [Question 1]
-    a. [Option 1]
-    b. [Option 2]
-    c. [Option 3]
-    d. [Option 4]
+    Questions:
+    ~question~ 1. [Question 1]
+        a. [Option 1]
+        b. [Option 2]
+        c. [Option 3]
+        d. [Option 4]
 
-~question~ 2. [Question 2]
-    a. [Option 1]
-    b. [Option 2]
-    c. [Option 3]
-    d. [Option 4]
+    ~question~ 2. [Question 2]
+        a. [Option 1]
+        b. [Option 2]
+        c. [Option 3]
+        d. [Option 4]
 
-...
+    ...
 
-Answer Key:
-1. Correct Answer for Question 1
-2. Correct Answer for Question 2
-...
+    Answer Key:
+    1. Correct Answer for Question 1
+    2. Correct Answer for Question 2
+    ...
 
-Ensure that the questions cover a range of topics and are of {difficulty} difficulty level."""}
+    Ensure that the questions cover a range of topics and are of {difficulty} difficulty level."""}
         ]
     )
     return completion.choices[0].message.content
--- backend/qna/router.py
+++ backend/qna/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response, Request
 from pydantic import BaseModel
 from azure.cosmos.cosmos_client import CosmosClient
 from azure.storage.blob import ContainerClient
@@ -7,6 +7,8 @@ import os
 from .pdf import content_to_pdf
 from model.qna2 import prompt
 from utils.response_to_json import response_to_json
+from users.router import get_user
+from uuid import uuid4
 
 qna_router = APIRouter(
     prefix="/qna",
@@ -17,6 +19,11 @@ def get_container():
     client = CosmosClient(os.environ.get('COSMOS_HOST'), { 'masterKey': os.environ.get('COSMOS_KEY') })
     db = client.get_database_client(os.environ.get('COSMOS_DB_ID'))
     return db.get_container_client('Modules')
+
+def get_quiz_container():
+    client = CosmosClient(os.environ.get('COSMOS_HOST'), { 'masterKey': os.environ.get('COSMOS_KEY') })
+    db = client.get_database_client(os.environ.get('COSMOS_DB_ID'))
+    return db.get_container_client('Quizzes')
 
 def get_storage():
     return ContainerClient(os.environ.get('STORAGE_NAME'), "quiz", credential=os.environ.get("STORAGE_CORR"))
@@ -36,7 +43,7 @@ async def get_available_mods():
     ))
 
 @qna_router.get("/")
-async def get_qna(mod: str, difficulty: str):
+async def get_qna(mod: str, difficulty: str, request: Request):
     if difficulty not in ("easy", "medium", "hard"):
         return {
             "detail": "Invalid difficulty level",
@@ -62,8 +69,22 @@ async def get_qna(mod: str, difficulty: str):
     pdf_question_id = content_to_pdf(questions, mod, "Questions", storage)
     pdf_answer_id = content_to_pdf(answers, mod, "Answers", storage)
 
-    return {
+    quiz_id = str(uuid4())
+    user = get_user(request)
+    get_quiz_container().create_item(body={
+        "id": quiz_id,
         "mod": mod,
+        "difficulty": difficulty,
+        **qna_json,
+        "qid": pdf_question_id,
+        "aid": pdf_answer_id,
+        "user": user.username if user is not None else None,
+    })
+
+    return {
+        "id": quiz_id,
+        "mod": mod,
+        "difficulty": difficulty,
         **qna_json,
         "qid": pdf_question_id,
         "aid": pdf_answer_id,
@@ -78,3 +99,57 @@ async def get_pdf(pdf_id: str):
         return {
             "message": "Not found",
         }
+
+@qna_router.get("/quiz")
+async def retrieve_quiz(quiz_id: str):
+    items = list(get_quiz_container().query_items(
+        query="SELECT * FROM r WHERE r.id=@id",
+        parameters=[
+            {
+                "name": "@id",
+                "value": quiz_id,
+            },
+        ],
+        enable_cross_partition_query=True,
+    ))
+    if len(items) == 0:
+        return {
+            "message": "Not found",
+        }
+    
+    item = items[0]
+    return {
+        "id": item["id"],
+        "mod": item["mod"],
+        "difficulty": item["difficulty"],
+        "questions": item["questions"],
+        "answers": item["answers"],
+        "qid": item["qid"],
+        "aid": item["aid"],
+    }
+
+@qna_router.get("/user-quizzes")
+async def get_user_quizzes(request: Request):
+    user = get_user(request)
+    if user is None:
+        return {
+            "message": "Not logged in",
+        }
+    
+    items = list(get_quiz_container().query_items(
+        query="SELECT * FROM r WHERE r.user=@user",
+        parameters=[
+            {
+                "name": "@user",
+                "value": user.username,
+            },
+        ],
+        enable_cross_partition_query=True,
+    ))
+
+    return {
+        "quizzes": list(map(
+            lambda item: item["id"],
+            items,
+        ))
+    }
